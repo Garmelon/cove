5cf0f7647751e99dc0ec90f2f4b207d5b066e75e
Joscha <joscha@plugh.de>
2022-02-07 22:44:30 +0100

    Print simple HelloCmd packet

--- cove-core/src/lib.rs
+++ cove-core/src/lib.rs
@@ -1,8 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct HelloCmd {
+    pub name: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct HelloRpl {
+    pub msg: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(tag = "type")]
+pub enum Data {
+    HelloCmd(HelloCmd),
+    HelloRpl(HelloRpl),
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Packet {
+    pub id: u64,
+    #[serde(flatten)]
+    pub data: Data,
 }
--- cove-server/src/main.rs
+++ cove-server/src/main.rs
@@ -1,3 +1,12 @@
+use cove_core::{Data, HelloCmd, Packet};
+
 fn main() {
     println!("Hello, world!");
+    let packet = Packet {
+        id: 1337,
+        data: Data::HelloCmd(HelloCmd {
+            name: "Garmy".to_string(),
+        }),
+    };
+    println!("{}", serde_json::to_string(&packet).unwrap());
 }
