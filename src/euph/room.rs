5b28d1601d112c6614d1f45be20520897047b2d9
Joscha <joscha@plugh.de>
2022-06-24 23:04:05 +0200

    Request logs every 2 seconds instead of 3

--- src/euph/room.rs
+++ src/euph/room.rs
@@ -97,7 +97,7 @@ impl State {
 
     async fn regularly_request_logs(event_tx: &mpsc::UnboundedSender<Event>) {
         loop {
-            time::sleep(Duration::from_secs(3)).await; // TODO Make configurable
+            time::sleep(Duration::from_secs(2)).await; // TODO Make configurable
             let _ = event_tx.send(Event::RequestLogs);
         }
     }
