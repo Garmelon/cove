42c23d6745454c7a52468e17db46fcea5494abef
Joscha <joscha@plugh.de>
2022-07-20 20:38:22 +0200

    Fix crash if no external editor is available

--- src/ui/util.rs
+++ src/ui/util.rs
@@ -7,10 +7,13 @@ pub fn prompt(terminal: &mut Terminal, crossterm_lock: &Arc<FairMutex<()>>) -> O
     let content = {
         let _guard = crossterm_lock.lock();
         terminal.suspend().expect("could not suspend");
-        let content = edit::edit("").expect("could not edit");
+        let content = edit::edit("");
         terminal.unsuspend().expect("could not unsuspend");
         content
     };
+
+    // TODO Don't swipe this error under the rug
+    let content = content.ok()?;
 
     if content.trim().is_empty() {
         None
