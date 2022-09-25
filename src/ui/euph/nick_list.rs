30276fcbbfc8e355367b50f48077354bdc217f50
Joscha <joscha@plugh.de>
2022-09-25 21:59:15 +0200

    Display nick list cursor

--- src/ui/euph/nick_list.rs
+++ src/ui/euph/nick_list.rs
@@ -13,8 +13,7 @@ use crate::ui::widgets::text::Text;
 use crate::ui::widgets::BoxedWidget;
 
 pub fn widget(state: &ListState<String>, joined: &Joined, focused: bool) -> BoxedWidget {
-    // TODO Handle focus
-    let mut list = state.widget();
+    let mut list = state.widget().focus(focused);
     render_rows(&mut list, joined);
     list.into()
 }
